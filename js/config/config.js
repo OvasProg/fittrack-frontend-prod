a88cd0d4428cb7dea1a93492cd7c14372745c326
OvasProg <alex.vasyliev.prog@gmail.com>
2026-03-24 22:22:11 +0200

    Include correct backend base url

--- js/config/config.js
+++ js/config/config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-   //API_URL: "http://localhost:8000",
-   //API_URL: "http://127.0.0.1:8000",
-   API_URL: "https://74egbc8e12.execute-api.eu-central-1.amazonaws.com",
-   USE_MOCK: false,
+  //API_URL: "http://localhost:8000",
+  //API_URL: "http://127.0.0.1:8000",
+  API_URL: "https://api.fittrack-app.online",
+  USE_MOCK: false,
 
-   IMG: {
-      workouts: "/img/workouts",
-      dashboard: "/img/dashboard",
-      sidebar: "/img/sidebar",
-      header: "/img/header",
-   },
+  IMG: {
+    workouts: "/img/workouts",
+    dashboard: "/img/dashboard",
+    sidebar: "/img/sidebar",
+    header: "/img/header",
+  },
 };
